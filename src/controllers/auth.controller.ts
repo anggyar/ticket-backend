f35cb3064a2549838508f162d6082a6bcafd37b0
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-10-14 01:22:21 +0700

    add Swagger request body and security definitions for login and me endpoints

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -69,6 +69,12 @@ export default {
   },
 
   async login(req: Request, res: Response) {
+    /**
+     #swagger.requestBody = {
+        required: true,
+        schema: {$ref: "#/components/schemas/LoginRequest"}
+     }
+     */
     const { identifier, password } = req.body as unknown as TLogin;
 
     try {
@@ -126,6 +132,11 @@ export default {
   },
 
   async me(req: IReqUser, res: Response) {
+    /**
+      #swagger.security = [{
+          "bearerAuth": []
+      }]
+     */
     try {
       const user = req.user;
       const result = await UserModel.findById(user?.id);
