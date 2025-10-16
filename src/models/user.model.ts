ccc3b65bbdfffef067c693e813f4768541e9a96c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-10-16 16:58:53 +0700

    add email notification on user registration: send activation email after saving user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import { encrypt } from "../utils/encryption";
+import { sendMail, renderMailHtml } from "../utils/mail/mail";
+import { name } from "ejs";
+import { CLIENT_HOST, EMAIL_SMTP_USER } from "../utils/env";
 
 export interface User {
   fullName: string;
@@ -10,6 +13,7 @@ export interface User {
   profilePicture: string;
   isActive: boolean;
   activationCode: string;
+  createdAt?: string;
 }
 
 const Schema = mongoose.Schema;
@@ -60,6 +64,33 @@ UserSchema.pre("save", function (next) {
   user.password = encrypt(user.password);
 
   next();
+});
+
+UserSchema.post("save", async function (doc, next) {
+  try {
+    const user = doc;
+
+    console.log("Send Email to: ", user.email);
+
+    const contentMail = await renderMailHtml("registration-success.ejs", {
+      username: user.username,
+      fullName: user.fullName,
+      email: user.email,
+      createdAt: user.createdAt,
+      activationLink: `${CLIENT_HOST}/auth/activation?code=${user.activationCode}`,
+    });
+
+    await sendMail({
+      from: EMAIL_SMTP_USER,
+      to: user.email,
+      subject: "Aktivasi Akun Anda",
+      html: contentMail,
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    next();
+  }
 });
 
 // Method to hide password when returning user object
